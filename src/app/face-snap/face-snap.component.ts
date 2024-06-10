bc87e8b6b2ad8a59ba18b72e205731af236de7c9
MrBabon <christophedanna06@gmail.com>
2024-06-10 09:13:01 +0200

    start

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  snaps!: number;
+  
+  ngOnInit(): void {
+    
+  }
+}
