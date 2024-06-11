4dd1b439fc843d410c4ed1ddfc877fd279827e4d
MrBabon <christophedanna06@gmail.com>
2024-06-11 21:27:16 +0200

    utilisation des pipe

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
-import { NgClass, NgStyle, UpperCasePipe } from '@angular/common';
+import { DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
@@ -8,7 +8,8 @@ import { NgClass, NgStyle, UpperCasePipe } from '@angular/common';
   imports: [
     NgStyle,
     NgClass,
-    UpperCasePipe
+    UpperCasePipe,
+    DatePipe
   ],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
